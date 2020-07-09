f5efa9bc6af41057ba60503224213e1ff9b8e29f
innapau <jemmek364@gmail.com>
2020-07-09 17:57:19 +0300

    Add a completed task  from HW#6

---
+++ hw6/task2/public/js/script.js
@@ -0,0 +1,271 @@
+const NotFound = {
+    template: '<h2>404 - Page Not Found</h2>'
+};
+
+//tab-info
+
+const Info = {
+    template: `<div class="tabs-panel">
+                <img src="./img/Miles_Davis.jpg" class="info-pic">
+                <div class="info-text">
+                <h2>Майлз Дэвис</h2>
+                <p>(<i>англ.</i> <strong>Miles Davis</strong>. 26 мая 1926, Олтон — 28 сентября 1991, Санта-Моника) — американский джазовый трубач и бэнд-лидер, оказавший значительнейшее влияние на развитие музыки XX века. Дэвис стоял у истоков множества стилей и направлений в джазе, таких как модальный джаз (англ. modal jazz), кул-джаз (cool jazz) и фьюжн (fusion).</p>
+                <p>Около 50 лет работы Дэвиса определили звучание джаза для трёх поколений слушателей. Записи Дэвиса позволяют проследить развитие современного джаза от бибопа конца 1940-х годов до современных экспериментальных направлений. В отличие от многих других джазменов, Дэвис никогда не был ограничен каким-то одним джазовым стилем. Отмечая широту интересов и многогранность его таланта, Дюк Эллингтон сравнивал Дэвиса с Пикассо.</p>
+                </div>
+                </div>`
+};
+
+//tab-biography
+
+const Biography = {
+    template: `<div class="tabs-panel">
+        <div class="info-text">
+            <h2>Биография</h2>
+            <h3>Ранний период творчества</h3>
+            <p>Майлз Дьюи Дэвис III (англ. Miles Dewey Davis III) родился и вырос в штате Иллинойс.</p>
+            <p>Начал играть на трубе в 13 лет. Намеревался стать академическим исполнителем, однако довольно быстро добился признания как джазмен, выступая с ансамблями Сонни Стита и Кларка Терри. В 1945, учась в Джульярдской музыкальной школе в Нью-Йорке, посещал джем-сейшнз на 52-й улице. В ноябре этого же года вошёл в квинтет Чарли Паркера, записав «Now’s The Time» и «Billie’s Bounce». Не обладая виртуозной техникой, Майлс Дэвис постепенно вырабатывал собственное звучание — длинные ноты без вибрато, частое использование сурдины, короткие фразы и многозначительные паузы. По мнению критика Андре Одэра: «Дэвис придал музыке Паркера интимный характер».</p>
+            <p>В конце 1945 года уехал в Сент-Луис, чтобы присоединиться к оркестру Бенни Картера, но работа в биг-бэнде ему не понравилась и поэтому он через несколько месяцев вернулся в Нью-Йорк и вновь играл с Паркером. В 1946 заменил Диззи Гиллеспи в оркестре Билли Экстайна, в то же время выступал с Сонни Ститом и Джином Аммонсом. В 1948 судьба опять свела его с Паркером, кроме того его партнёрами стали Сонни Роллинз, Джон Льюис и Макс Роуч.</p>
+            <h3>Период cool jazz</h3>
+            <p>В 1948 году собрал нонет, который вошёл в историю джаза как CAPITOL BAND — первый ансамбль так называемого «прохладного джаза». Состав нонета был для того времени достаточно оригинален — труба, тромбон, валторна, туба, альт- и баритон-саксофон, фортепиано, контрабас, ударные. Термин «кул» впервые появился на пластинке Дэвиса «The Birth Of The Cool» (1949), на которой были записаны композиции и аранжировки Джерри Маллигэна, Джона Льюиса, Гила Эванса и Джона Каризи.</p>
+            <h3>1950-е годы, «третье течение»</h3>
+            <p>Карьера Дэвиса была неровной, в 1950-е годы он увлёкся наркотиками и после записей 1950—1951 годов с составом METRONOME ALL STARS не появлялся на сцене 4 года. Только в 1955 году он собрал ансамбль, в который вошли Джон Колтрейн, Рэд Гарлэнд (англ.)русск., Пол Чемберс (англ.)русск., Филли Джо Джонс (иногда присоединялись Кэннонбол Эддерли и Билл Эванс). Во второй половине 1950-х годов Дэвис становится признанным лидером современного джаза и начинает экспериментировать в области формы и гармонии, заменяя традиционные аккордовые последовательности остинатными басовыми формулами (альбомы Milestones, 1958, Kind Of Blue, 1959). В то же время он записывает (на трубе и флюгельгорне) с оркестром Гила Эванса сюиты, которые можно отнести к «третьему течению» — Miles Ahead (1957), Porgy and Bess (1958), Sketches of Spain (1959).</p>
+            <h3>Акустический джаз, европейские гастроли</h3>
+            <p>
+              В 1960-е годы Дэвис собрал новый квинтет, в который пригласил молодых виртуозов — Уэйна Шортера, Херби Хэнкока, Рона Картера, Тони Уильямса, триумфально гастролировал в Европе и выпустил альбомы, которые считаются вершиной развития акустического джаза — Davis In Europe (1963), Seven Steps to Heaven (1963), My Funny Valentine (1965), E.S.P. (1965), Miles Smiles (1967) и другие.
+            </p>
+            <h3>Период джаз-рока</h3>
+            <p>
+              Новый виток популярности начался в конце 1960-х годов, когда Дэвис обратил внимание на электроинструменты, на энергетику рок-музыки, отказался от использования тем, напоминающих джазовые стандарты, разработал новый тип аранжировки (продолжительные композиции цементировались короткими риффами) и пригласил в свою группу молодых исполнителей — Джо Завинула, Джона Маклафлина, Чика Кориа, Дэйва Холланда, Ленни Уайта (англ.)русск., Билли Кобэма, Стива Гроссмана (англ.)русск., Аирто Морейру. С ними он записал ряд джаз-роковых альбомов — Filles De Kilimanjaro (1968), In a Silent Way (1969) и Bitches Brew (1970). В 1970-е годы Дэвис утвердил на сцене новый тип джазового шоу, после чего стал частым гостем американских и европейских джаз- и рок-фестивалей.</p>
+              <p>Однако здоровье Дэвиса ухудшалось. Смерть близких ему музыкантов, воспаление лёгких, автомобильная авария и повреждение коленного сустава, наркотики и неприятности с полицией — всё это привело к сильной депрессии, и после 1975 года он не появлялся на сцене 6 лет.
+              </p>
+              <h3>Фанк-рок джаз</h3>
+              <p>
+              О выздоровлении и возвращении возвестил альбом «The Man With A Horn» (1981). В оркестре вновь появились новые имена (он всегда умел открывать таланты) — Джон Скофилд, Кенни Гарретт, Майк Стерн (англ.)русск., Эл Фостер, Маркус Миллер, Боб Берг (англ.)русск. и другие. Новый стиль, который можно охарактеризовать как фанк-рок-джаз, был представлен альбомом «Tutu» (1986); Майк Зверин назвал его «звуковой дорожкой десятилетия»[где?]. Дэвис продолжал гастролировать вплоть до своей кончины в 1991 году.
+            </p>
+            <h3>Личная жизнь</h3>
+            <p>
+             С 1981 по 1988 год был женат на американской актрисе Сисели Тайсон.
+            </p>
+        </div>
+    </div>`
+};
+
+//tab-contribution
+
+const Contribution = {
+    template: `<div class="tabs-panel">
+                <div class="info-text">
+                    <h2>Вклад в развитие музыки</h2>
+                    <p>Среди его главных достижений:</p>
+                    <ul>
+                        <li>Cоздание оригинального стиля игры на трубе, который впервые проявился в записях, сделанных Дэвисом с Чарли Паркером в середине 1940-х годов и который оказал влияние на огромное количество трубачей, играющих в стиле бибоп и кул (прохладный джаз);</li>
+                        <li>Майлс Дэвис стоял у истоков таких направлений в джазе, как фьюжн и джаз-рок;</li>
+                        <li>Запись основополагающих альбомов для т. н. прохладного джаза — Birth of the Cool (1949), модального джаза — Kind of Blue (1959), стиля джаз-фьюжн — In a Silent Way и Bitches Brew (оба записаны в 1969 году).</li>
+                        <li>Альбом «In a Silent Way» в 2002 году был включён в рейтинг The 25 Most Influential Ambient Albums Of All Time — список альбомов, оказавших наибольшее влияние на развитие эмбиента.</li>
+                    </ul>
+                </div>
+            </div>`
+};
+
+//tab-discography
+
+const Discography = {
+    props: ['albums'],
+    template: `<div class="tabs-panel">
+                <div class="discography" :albums="albums">
+                    <div class="album" v-for="album in albums"
+                                    :key="album.id"
+                                    v-if="!$route.params.shortcut"
+                                    >
+                        <router-link :to="{name: 'albumInfo', params: {shortcut: album.shortcut}}" ><img :src="album.img"></router-link>
+                        <h3>{{album.title}}</h3>
+                        <p>{{album.release}}</p>
+                        <p>{{album.songs.length}} композиций</p>
+                    </div>
+                    <router-view v-else-if="$route.params.shortcut == album.shortcut" :album="album"></router-view>
+                </div>
+            </div>`
+};
+
+const Album = {
+    props: ['album'],
+    template: `<div class="album-info">
+                    <div class="top-container">
+                        <div class="album-info-text">
+                            <h2>{{album.title}}</h2>
+                            <p>Дата релиза: <span class="desc">{{album.release}}</span></p>
+                            <p>Продолжительность: <span class="desc">{{durationSum}}</span></p>
+                        </div>
+                        <div class="album-image">
+                            <img :src="album.img">
+                        </div>
+                    </div>
+                    <div class="songlist">
+                        <ol>
+                            <li v-for="song in album.songs" class="song "><span>{{song.title}}</span><span class="song-duration">{{song.duration}}</span></li>
+                        </ol>
+                    </div>
+            </div>`,
+    computed: {
+        durationSum() {
+            let that = this;
+            let time = moment.duration('');
+            for (let i = 0; i < that.album.songs.length; i++) {
+                time.add(that.album.songs[i].duration);
+            }
+            let h = time.hours();
+            let m = time.minutes();
+            let s = time.seconds();
+            if (m < 10) {
+                m = '0' + m;
+            } else if (s < 10) {
+                s = '0' + s
+            }
+            return `${h}:${m}:${s}`;
+        }
+    }
+};
+
+const routes = [
+    {path: '/',name: '/', component: Info},
+    {path: '/biography', name: 'biography', component: Biography},
+    {path: '/contribution', name: 'contribution', component: Contribution},
+    {path: '/discography', name: 'discography', component: Discography,
+        children: [
+            {path: ':shortcut', name: 'albumInfo', component: Album},
+        ]
+    },
+    {path: '/404', component: NotFound},
+    {path: '*', redirect: '/404'}
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+})
+
+let app = new Vue({
+    el: '#app',
+    components: {
+        'info': Info,
+        'biography': Biography,
+        'contribution': Contribution,
+        'discography': Discography,
+        'album': Album
+    },
+    data: {
+        tabTitles: [{
+            name: 'О музыканте',
+            shortcut: '/'
+        }, {
+            name: 'Биография',
+            shortcut: 'biography'
+        }, {
+            name: 'Лепта',
+            shortcut: 'contribution'
+        },{
+            name: 'Дискография',
+            shortcut: 'discography'
+        }],
+        currentTab: 'info',
+        albums: [{
+            id: 1,
+            title: 'Kind of Blue',
+            shortcut: 'kindofblue',
+            img: '/img/kind-of-blue.jpg',
+            release: '17 Августа 1959',
+            songs: [{title: 'So What', duration: '00:07:57'},
+                    {title: 'Freddie Freeloader', duration: '00:07:09'},
+                    {title: 'Blue in Green', duration: '00:03:50'},
+                    {title: 'All Blues', duration: '00:07:50'},
+                    {title: 'Flamenco Sketches', duration: '00:09:19'}]
+        }, {
+            id: 2,
+            title: 'Bitches Brew',
+            shortcut: 'bithcesbrew',
+            img: '/img/bitches-brew.jpg',
+            release: 'Апрель 1970',
+            songs: [{title: "Pharaoh's Dance", duration: '00:20:04'},
+                    {title: 'Bitches Brew', duration: '00:27:00'},
+                    {title: 'Spanish Key', duration: '00:17:28'},
+                    {title: 'John McLaughlin', duration: '00:04:23'},
+                    {title: 'Miles Runs the Voodoo Down', duration: '00:14:01'},
+                    {title: 'Sanctuary', duration: '00:08:50'}]
+        }, {
+            id: 3,
+            title: 'Birth of the Cool',
+            shortcut: 'birthofthecool',
+            img: '/img/birth-of-the-cool.jpg',
+            release: '1 Января 1957',
+            songs: [{title: "Move", duration: '00:02:34'},
+                    {title: 'Jeru', duration: '00:02:30'},
+                    {title: 'Moon Dreams', duration: '00:03:22'},
+                    {title: 'Venus de Milo', duration: '00:03:14'},
+                    {title: 'Budo', duration: '00:02:37'},
+                    {title: 'Deception', duration: '00:02:48'},
+                    {title: 'Godchild', duration: '00:02:51'},
+                    {title: 'Boplicity', duration: '00:03:04'},
+                    {title: 'Rocker', duration: '00:03:04'},
+                    {title: 'Israel', duration: '00:02:15'},
+                    {title: 'Rogue', duration: '00:03:13'}]
+        }, {
+            id: 4,
+            title: 'Milestones',
+            shortcut: 'milestones',
+            img: '/img/milestones.jpg',
+            release: 'Апрель 1958',
+            songs: [{title: "Dr. Jackle", duration: '00:05:50'},
+                    {title: "Sid's Ahead", duration: '00:12:50'},
+                    {title: 'Two Bass Hit', duration: '00:05:49'},
+                    {title: 'Milestones', duration: '00:04:10'},
+                    {title: 'Billy Boy', duration: '00:07:13'},
+                    {title: 'Straight, No Chaser', duration: '00:10:56'},
+                    {title: 'Two Bass Hit (Alternate Take)', duration: '00:04:29'},
+                    {title: 'Milestones (Alternate Take)', duration: '00:05:59'},
+                    {title: 'Straight, No Chaser (Alternate Take)', duration: '00:10:27'}]
+        }, {
+            id: 5,
+            title: 'Doo-Bop',
+            shortcut: 'doo-bop',
+            img: '/img/doo-bop.jpg',
+            release: '25 июня 1992',
+            songs: [{title: "Mystery", duration: '00:03:55'},
+                    {title: 'The Doo-Bop Song', duration: '00:05:00'},
+                    {title: 'Chocolate Chip', duration: '00:04:38'},
+                    {title: 'High Speed Chase', duration: '00:04:41'},
+                    {title: 'Blow', duration: '00:05:06'},
+                    {title: 'Sonya', duration: '00:05:31'},
+                    {title: 'Fantasy', duration: '00:04:35'},
+                    {title: 'Duke Booty', duration: '00:04:55'},
+                    {title: 'Mystery (reprise)', duration: '00:01:29'}]
+        }, {
+            id: 6,
+            title: 'Miles Ahead',
+            shortcut: 'milesahead',
+            img: '/img/miles-ahead.jpg',
+            release: '1957',
+            songs: [{title: "Springsville", duration: '00:03:27'},
+                    {title: 'The Maids of Cadiz', duration: '00:03:59'},
+                    {title: 'The Duke', duration: '00:03:34'},
+                    {title: 'My Ship', duration: '00:04:28'},
+                    {title: 'Miles Ahead', duration: '00:03:32'},
+                    {title: 'Blues for Pablo', duration: '00:05:19'},
+                    {title: 'New Rhumba', duration: '00:04:24'},
+                    {title: 'The Meaning of the Blues', duration: '00:02:40'},
+                    {title: 'Lament', duration: '00:02:16'},
+                    {title: "I Don't Wanna Be Kissed", duration: '00:03:12'}]
+        }]
+    },
+    router: router,
+    computed: {
+        currentTabComponent() {
+			return this.currentTab;
+		},
+        tabName() {
+            if (this.tabTitles.shortcut === 'info') {
+                return '/';
+            }
+            return this.tabTitles.shortcut;
+        }
+    }
+});
